a330925e3195316de0cf00910d89321721de9c49
Pankaj Rana <pankaj.rana@ihsmarkit.com>
2023-06-05 14:49:40 +0530

    practice

---
+++ random-practice/src/com/pankaj/TypeCasting.java
@@ -0,0 +1,10 @@
+package com.pankaj;
+class TypeCasting{
+    public static void main(String...args){
+        
+            long l=123456789L;
+            byte b=(byte)l;
+            l=b;
+            System.out.println(b);            
+    }
+}
